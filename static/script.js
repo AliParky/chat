f5e3d37d654383c1ee0461aeff4503115e7f70cc
AliParky <aparkinson670@gmail.com>
2024-03-14 13:16:51 +0800

    Add message display functionality to script.js

--- static/script.js
+++ static/script.js
@@ -1,7 +1,16 @@
 // Establish Socket.IO Connection
 const socket = io();
 
+const messagesList = document.getElementById('messages');
+
 messageForm.addEventListener('submit', (event) => {
     // Send message to the server
     socket.emit('message', { message: message });
+});
+
+// Receive New Messages
+socket.on('new_message', (data) => {
+    const newMessage = document.createElement('li');
+    newMessage.textContent = data.message;
+    messagesList.prepend(newMessage); // Add new messages to the top
 });
