02580d7e4498878fe20cd9e5ca0e455b89be7ec6
AliParky <aparkinson670@gmail.com>
2024-03-15 15:22:19 +0800

    Add message input clearing functionality

--- static/script.js
+++ static/script.js
@@ -4,6 +4,9 @@ const socket = io();
 const messagesList = document.getElementById('messages');
 
 messageForm.addEventListener('submit', (event) => {
+    const message = messageInput.value;
+    messageInput.value = ''; // Clear the input field
+
     // Send message to the server
     socket.emit('message', { message: message });
 });
