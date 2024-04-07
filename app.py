684f36f9beb90fb505cfd91aeb23ebf5014b15dd
AliParky <aparkinson670@gmail.com>
2024-04-07 17:36:55 +0800

    Switch from SQLite to PostgreSQL

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you_will_never_guess'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 socketio = SocketIO(app)
