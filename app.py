1e89fbed086eb2090c3e062a44e623658e426b7c
AliParky <aparkinson670@gmail.com>
2024-04-12 21:48:06 +0800

    Change database URI to use "postgresql://" instead of "postgres://"

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'you_will_never_guess'
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL'].replace("postgres://", "postgresql://", 1)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     return app
